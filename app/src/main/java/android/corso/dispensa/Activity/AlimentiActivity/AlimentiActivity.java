b169a9272cacc8a11c5cb8351c6ec16ae4730a5e
AndreaDagg <daguannoandrea@outlook.com>
2019-10-19 23:11:33 +0200

    bk1

--- app/src/main/java/android/corso/dispensa/Activity/AlimentiActivity/AlimentiActivity.java
+++ app/src/main/java/android/corso/dispensa/Activity/AlimentiActivity/AlimentiActivity.java
@@ -2,8 +2,11 @@ package android.corso.dispensa.Activity.AlimentiActivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.corso.dispensa.R;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AlimentiActivity extends AppCompatActivity {
 
@@ -11,5 +14,34 @@ public class AlimentiActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alimenti);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        setButtonOpenDipensaAlimenti();
+        setButtonAddFood();
+
+    }
+
+    private void setButtonOpenDipensaAlimenti() {
+        Button buttonOpenDispAli = findViewById(R.id.buttonOpenDispAli);
+        buttonOpenDispAli.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), DispensaAlimentiActivity.class));
+            }
+        });
+    }
+
+    private void setButtonAddFood() {
+        Button buttonAddFood = findViewById(R.id.buttonAddAli);
+        buttonAddFood.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), NuovoAlimentoActivity.class));
+            }
+        });
     }
 }
